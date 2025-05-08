a0b2aceaa800dfdae150a95457e163c5a0c2eb61
riaurko <riaurko@gmail.com>
2025-05-08 23:00:12 +0600

    Initialized Project

---
+++ src/pages/Root.jsx
@@ -0,0 +1,11 @@
+import { Fragment } from 'react';
+
+const Root = () => {
+    return (
+        <Fragment>
+            <h1 className="text-5xl font-bold text-center mt-8">ROOT</h1>
+        </Fragment>
+    );
+};
+
+export default Root;
